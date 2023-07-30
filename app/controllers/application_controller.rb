1a4270b4b485bd2da1c1b8dedc3f3293824ddbf5
Enock Ngetich <ngetichenock32@gmail.com>
2023-07-30 03:12:59 +0300

    Authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
-class ApplicationController < ActionController::API
-    include ActionController::Cookies
-|
-	private
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
 
-	def current_user
+  before_action :authenticate_user!
+
+  private
+
+  def current_user
     @current_user ||= User.find_by_id(session[:user_id])
   end
 end
