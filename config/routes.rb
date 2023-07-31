abfc6d591bc32161d7d243ecd0c960ebd8db623a
Enock Ngetich <ngetichenock32@gmail.com>
2023-07-31 16:28:15 +0300

    Sign Up

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ Rails.application.routes.draw do
   root "articles#index"
   get '/hello', to: 'application#hello_world'
   post '/login', to: 'sessions#create'
+  post '/signup', to: 'sessions#crate'
   delete '/logout', to: 'sessions#destroy'
   get '/me', to: 'users#show'
   get '/saved_jobs/:id', to: 'users#saved_jobs'
