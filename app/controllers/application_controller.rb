7d38fb280a02c2aa4f3670f9bed1c9d6919979f7
Enock Ngetich <ngetichenock32@gmail.com>
2023-07-31 12:16:28 +0300

    Application Controller Fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   include ActionController::Cookies
-  # protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
   # before_action :authenticate_user!
 
