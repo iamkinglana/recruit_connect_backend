d6cfbeac1ec2003d6007156cd77668be09614fbb
Enock Ngetich <ngetichenock32@gmail.com>
2023-08-01 23:49:20 +0300

    Create Fix

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
   root "articles#index"
   get '/hello', to: 'application#hello_world'
   post '/login', to: 'sessions#create'
-  post '/signup', to: 'sessions#crate'
+  post '/signup', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/me', to: 'users#show'
   get '/saved_jobs/:id', to: 'users#saved_jobs'
